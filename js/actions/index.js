391597203ccb9d303e3c6e5b24d0aba1ae25b147
Faras Maroge <Goldencrane33@yahoo.com>
2017-02-25 14:43:58 -0500

    added the appropriate actions for the game

---
+++ js/actions/index.js
@@ -0,0 +1,28 @@
+//Add a comment to the top of the file listing the various things that you need to be able to do during a game.
+
+//make an action property
+const action = {
+        type: 'pass_ball',
+        content: 'the actions of the field'
+    }
+    //For example, one action might be to guess a number. Another action might generate a new random number.
+
+
+//Add an action creator function for each of the actions that you've listed.
+
+cont pass_ball = 'pass_ball';
+const action {
+    type: pass_ball,
+
+}
+
+//make an action property
+const action = {
+    type: 'generate_number',
+    content: 'the actions of the generating a number'
+}
+
+const action = {
+    type: 'guess_number',
+    content: 'the actions of guessing a random number'
+}
