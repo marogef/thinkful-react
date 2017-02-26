0f195b950da59f52e027316816674c67e20c9afb
Faras Maroge <Goldencrane33@yahoo.com>
2017-02-26 18:24:30 -0500

    added testing, gallery components

---
+++ js/components/image.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Image(props) {
+    return ( < div className = "gallery-image" >
+        < img src = {
+            props.url
+        }
+        alt = {
+            props.description
+        }
+        /> < p > {
+            props.description
+        } < /p> < /div>
+    );
+}
